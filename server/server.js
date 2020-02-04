34ac538b8fae9520416c46f723f2850e3e9ef909
MehdiMahjoub <mahjoub.mahdi5@gmail.com>
2020-02-04 15:52:31 +0100

    update server js file add middleware, add routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const session = require('express-session')
+
 
 // PORT init
 const PORT = process.env.PORT || 3000
 
+// Embedded JavaScript templates
+app.set('view engine', 'ejs')
+
+
+// Middleware
+
+// parse application/x-www-from-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use('/assets', express.static('public'))
+
+
+// routes
+app.get('/', (req, res) => {
+    res.render('pages/index')
+})
 
 // Server PORT listen
 app.listen(PORT, () => {
