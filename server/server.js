dd2ed6243a6c49137e1067eafa8d5c203c059e88
MehdiMahjoub <mahjoub.mahdi5@gmail.com>
2020-02-04 17:07:20 +0100

    update serverjs, add func check message, add message info in template html

--- server/server.js
+++ server/server.js
@@ -19,12 +19,36 @@ app.use(bodyParser.urlencoded({extended: false}))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(session({
+    secret: 'test',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
+
 app.use('/assets', express.static('public'))
 
 
 // routes
 app.get('/', (req, res) => {
+    if (req.session.error) {
+        res.locals.error = req.session.error
+        req.session.error = undefined
+    } else if (req.session.success) {
+        res.locals.success = req.session.success
+        req.session.success = undefined
+    }
     res.render('pages/index')
+})
+
+app.post('/', (req, res) => {
+    if (req.body.message === undefined || req.body.message === '') {
+        req.session.error = "il y a une erreur le formulaire est vide :("
+        res.redirect('/')
+    } else {
+        req.session.success = "Merci :)"
+        res.redirect('/')
+    }
 })
 
 // Server PORT listen
